90f89d37f2e72b9b3017bd66e181a5b1f4caaf8a
Aykhan Huseyn <immense_ideas@icloud.com>
2022-09-23 02:24:00 +0400

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import { ReactComponent as LocationIcon } from 'assets/images/location.svg';
 import frame from 'assets/images/frame.png';
 import caledar from '/caledar.ics';
 
+const location = 'https://goo.gl/maps/8HYdXvnnn3EFF2ku5';
+
 const frameStyle: CSSProperties = {
 	backgroundImage: `url(${frame})`,
 };
@@ -24,32 +26,6 @@ const containerStyle: CSSProperties = {
 	marginTop: '-30px',
 };
 
-function navigate(lat: number, lng: number) {
-	if (
-		navigator.platform.indexOf('iPhone') !== -1 ||
-		navigator.platform.indexOf('iPad') !== -1 ||
-		navigator.platform.indexOf('iPod') !== -1
-	) {
-		function iOSversion() {
-			if (/iP(hone|od|ad)/.test(navigator.platform)) {
-				// supports iOS 2.0 and later
-				var v = navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
-				return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
-			}
-		}
-		var ver = iOSversion() || [0];
-
-		var protocol = 'http://';
-		if (ver[0] >= 6) {
-			protocol = 'maps://';
-		}
-		window.location =
-			protocol + 'maps.apple.com/maps?place=' + lat + ',' + lng + '&amp;ll=';
-	} else {
-		window.open('http://maps.google.com?place=' + lat + ',' + lng + '&amp;ll=');
-	}
-}
-
 function App() {
 	const [theme, changeTheme] = useState<Theme>(light);
 
@@ -63,6 +39,7 @@ function App() {
 				maxWidth='477px'
 				minWidth='450px'
 				// minWidth='415px'
+				minHeight='680px'
 				maxHeight='750px'
 				fillAvailable
 				style={containerStyle}
@@ -114,15 +91,7 @@ function App() {
 					<Divider height='6px' />
 					Imperial Restaurant
 				</Text>
-				<Text
-					size='xs' // 40,68907° N, 46,38296° E
-					// 
-					// https://www.google.com/maps/place/%22Gold+Imperial%22+restoranı/@40.6894334,46.3830687,698m/data=!3m1!1e3!4m12!1m6!3m5!1s0x403f517da0c7bbc9:0x86d4aed2be8da797!2zSW1wZXJpYWwgxZ5hZGzEsXEgU2FyYXnEsQ!8m2!3d40.6907473!4d46.3841047!3m4!1s0x0:0xa5ed77983bea13ca!8m2!3d40.6889967!4d46.3828501
-					marginTop='md' // /@40.6894334,46.3830687,698m/data=!3m1!1e3!4m12!1m6!3m5!1s0x403f517da0c7bbc9:0x86d4aed2be8da797!2zSW1wZXJpYWwgxZ5hZGzEsXEgU2FyYXnEsQ!8m2!3d40.6907473!4d46.3841047!3m4!1s0x0:0xa5ed77983bea13ca!8m2!3d40.6889967!4d46.3828501
-					onClick={() => navigate(40.68907, 46.38296)}
-					hoverable
-					info
-				>
+				<Text as='a' href={location} marginTop='md' size='xs' hoverable info>
 					<LocationIcon
 						aria-labeledby='location'
 						style={{
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg' {
+	import React = require('react');
+	export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+	const src: string;
+	export default src;
+}
+
+declare module '*.ics' {
+	const src: string;
+	export default src;
+}
